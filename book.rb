0128a0e987ae71fb3ee6e0a5c741e34ef2b6cb3e
Lordkaito <cespedesisai88@gmail.com>
2021-12-16 23:43:28 +0100

    Change method from book to students

--- book.rb
+++ book.rb
@@ -13,9 +13,4 @@ class Book
   def add_rental(date, person)
     Rental.new(date, self, person)
   end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    @classroom.students.push(self) unless @classroom.students.include?(self)
-  end
 end
