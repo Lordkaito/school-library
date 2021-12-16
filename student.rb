402cd991c339b7010e16695a20281f6f36a4ee14
Lordkaito <cespedesisai88@gmail.com>
2021-12-17 00:29:25 +0100

    Correct attributes for some files

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
