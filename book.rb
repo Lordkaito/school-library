e56927be0fd33687b169d469c4375da6e899b6a3
Lordkaito <cespedesisai88@gmail.com>
2021-12-16 23:39:48 +0100

    Correct error with student

--- book.rb
+++ book.rb
@@ -13,4 +13,9 @@ class Book
   def add_rental(date, person)
     Rental.new(date, self, person)
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
+  end
 end
