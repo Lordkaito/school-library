86076ff1fe50901be250ee3d18c8b4ab7ea65943
Lordkaito <cespedesisai88@gmail.com>
2021-12-17 17:36:16 +0100

    Fix linter errors

--- main.rb
+++ main.rb
@@ -11,7 +11,7 @@ class Options
     @rental_arr = []
   end
 
-  def CreatePerson
+  def create_person
     puts 'Do you want to create a student[1] or teacher[2]?'
     option = Integer(gets.chomp)
     case option
@@ -35,7 +35,6 @@ class Options
         object: student
       })
       puts 'Student created!'
-      puts "\n"
     when 2
       print 'Age: '
       user_age = Integer(gets.chomp)
@@ -49,69 +48,67 @@ class Options
         object: teacher
       })
       puts 'Teacher created!'
-      puts "\n"
     else
       puts 'Invalid option!'
-      puts "\n"
     end
+  end
 
-    def CreateBook
-      print 'Title: '
-      book_title = gets.chomp
-      print 'Author: '
-      book_author = gets.chomp
-      puts 'Book created!'
+  def create_book
+    print 'Title: '
+    book_title = gets.chomp
+    print 'Author: '
+    book_author = gets.chomp
+    puts 'Book created!'
 
-      book = Book.new(book_title, book_author)
-      @books_arr.push({
-        output: "[Book] Title: #{book.title}, Author: #{book.author}",
-        object: book
-      })
+    book = Book.new(book_title, book_author)
+    @books_arr.push({
+    output: "[Book] Title: #{book.title}, Author: #{book.author}",
+    object: book
+    })
+  end
+
+  def book_list
+    @books_arr.each do |book|
+      puts book[:output]
     end
+  end
 
-    def BookList
-      @books_arr.each do |book|
-        puts book[:output]
-      end
+  def people_list
+    @person_arr.each do |person|
+      puts person[:output]
     end
+  end
 
-    def PeopleList
-      @person_arr.each do |person|
-        puts person[:output]
-      end
+  def create_rental
+    puts 'Select the number of the book you want to rent:'
+    @books_arr.each_with_index do |book, index|
+      puts "#{index + 1} - #{book[:output]}"
     end
+    book_number = Integer(gets.chomp)
+    book_selected = @books_arr[book_number - 1][:object]
 
-    def CreateRental
-      puts 'Select the number of the book you want to rent:'
-      @books_arr.each_with_index do |book, index|
-        puts "#{index + 1} - #{book[:output]}"
-      end
-      book_number = Integer(gets.chomp)
-      book_selected = @books_arr[book_number - 1][:object]
-
-      puts 'Select the number of the person to rent the book:'
-      @person_arr.each_with_index do |person, index|
-        puts "#{index + 1} - #{person[:output]}"
-      end
-      person_number = Integer(gets.chomp)
-      person_selected = @person_arr[person_number - 1][:object]
+    puts 'Select the number of the person to rent the book:'
+    @person_arr.each_with_index do |person, index|
+      puts "#{index + 1} - #{person[:output]}"
+    end
+    person_number = Integer(gets.chomp)
+    person_selected = @person_arr[person_number - 1][:object]
     print 'Date of rental: '
     rental_date = gets.chomp
     @rental_arr.push(Rental.new(rental_date, book_selected, person_selected))
 
     puts 'Rental created!'
-    end
+  end
 
-    def RentalList
-      print 'Choose the ID of the person you want to see the rentals: '
-      person_id = Integer(gets.chomp)
-      puts 'Rental: '
-      @rental_arr.each do |rental|
-        if rental.person.id == person_id
-          puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
-        else
-          puts 'No rentals found!'
-        end
+  def rental_list
+    print 'Choose the ID of the person you want to see the rentals: '
+    person_id = Integer(gets.chomp)
+    puts 'Rental: '
+    @rental_arr.each do |rental|
+      if rental.person.id == person_id
+        puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
+      else
+        puts 'No rentals found!'
       end
     end
   end
@@ -141,17 +138,17 @@ class ListOfOptions
   loop do
     case page_for_options
     when 1
-      option.BookList
+      option.book_list
     when 2
-      option.PeopleList
+      option.people_list
     when 3
-      option.CreatePerson
+      option.create_person
     when 4
-      option.CreateBook
+      option.create_book
     when 5
-      option.CreateRental
+      option.create_rental
     when 6
-      option.RentalList
+      option.rental_list
     when 7
       puts 'Goodbye!'
       exit
@@ -160,7 +157,7 @@ class ListOfOptions
 end
 
 def main
-  showOptions()
+  showOptions
 end
 
-main()
+main
